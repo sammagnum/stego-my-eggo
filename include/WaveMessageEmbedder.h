e89e8495f9423c39db0232f067bdbce23c935642
Sam Mangum <smangum@cs.utsa.edu>
2012-07-17 10:17:28 -0500

    WaveMessageEmbedder Class can take in necessary data and print the class

--- include/WaveMessageEmbedder.h
+++ include/WaveMessageEmbedder.h
@@ -15,13 +15,17 @@ class WaveMessageEmbedder
         std::bitset<8> * cover;
         DWORD mByteCount;
         DWORD cByteCount;
+
+        void setMessageByte(BYTE val,int cnt);
+        void setCoverByte(BYTE val,int cnt);
+
     public:
         WaveMessageEmbedder(char * message,unsigned int messageSize, BYTE * cover, DWORD coverSize);
         virtual ~WaveMessageEmbedder();
         std::bitset<8> * getMessage() { return message; }
-        void setMessageByte(std::bitset<8> val,int cnt) { message[cnt].set(val); }
         std::bitset<8> * getCover() { return cover; }
-        void setCoverByte(std::bitset<8> * val) {cover = val; }
+        void print();
+
 
 };
 
--- src/WaveMessageEmbedder.cpp
+++ src/WaveMessageEmbedder.cpp
@@ -1,14 +1,22 @@
 #include "..\include\WaveMessageEmbedder.h"
 
+using std::cout;
+using std::endl;
+
 WaveMessageEmbedder::WaveMessageEmbedder(char * m, unsigned int mSize, BYTE * c, DWORD cSize)
 {
-    int cnt = 0;
+    int cnt;
     mByteCount = mSize;
-    cByteCount =
+    cByteCount = cSize;
     message = new std::bitset<8>  [mSize];
     cover = new std::bitset<8>  [cSize];
+
     for( cnt = 0 ; cnt < mSize; cnt++)
         setMessageByte(m[cnt],cnt);
+
+    for( cnt = 0 ; cnt < cSize; cnt++)
+        setCoverByte(c[cnt],cnt);
+
 }
 
 WaveMessageEmbedder::~WaveMessageEmbedder()
@@ -17,20 +25,42 @@ WaveMessageEmbedder::~WaveMessageEmbedder()
     delete [] cover;
 }
 
-WaveMessageEmbedder::print()
+void WaveMessageEmbedder::print()
 {
+    int cnt;
     cout << "Message Bytes" << endl;
-    for( cnt = 0 ; cnt < mByteCount; cnt++)
-        std::cout<< message[cnt] << ", "
+    for( cnt = 0 ; cnt < mByteCount; cnt++){
+        cout<< message[cnt] << ", ";
+        if ( cnt % 10 == 0)
+            cout << endl;
+    }
+
+    cout << "Cover Bytes" << endl;
+    for( cnt = 0 ; cnt < cByteCount; cnt++){
+        cout<< cover[cnt] << ", ";
+        if ( cnt % 10 == 0)
+            cout << endl;
+    }
 
 }
 
+void WaveMessageEmbedder::setMessageByte(BYTE val,int cnt)
+{
+    std::bitset<8> temp (val);
+    message[cnt] = temp;
+}
+
+void WaveMessageEmbedder::setCoverByte(BYTE val,int cnt)
+{
+    std::bitset<8> temp (val);
+    cover[cnt] = temp;
+}
 int main()
 {
     char message [] = "abcd";
     BYTE cover []  = {1,255,1,255};
-    WaveMessageEmbedder * w  = new WaveMessageEmbedder(message,4,cover,4);
-    std::cout<< "test";
-    delete w;
+    WaveMessageEmbedder w (message,4,cover,4);
+    w.print();
+
     return 0;
 }
