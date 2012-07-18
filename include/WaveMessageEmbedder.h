d41c29e39a4b9a788036817ecd7262542bd5a8f4
Sam Mangum <smangum@cs.utsa.edu>
2012-07-18 13:44:57 -0500

    added averaging and message parsing methods

--- include/WaveMessageEmbedder.h
+++ include/WaveMessageEmbedder.h
@@ -4,27 +4,36 @@
 #include <iostream>
 #include <bitset>
 #include <fstream>
+#include <string>
+#include <vector>
+#include <math.h>
+
 
 #include "windows.h"
 
 
 class WaveMessageEmbedder
 {
-    private:
-        std::bitset<8> * message;
-        std::bitset<8> * cover;
-        DWORD mByteCount;
-        DWORD cByteCount;
+private:
+    std::vector<bool> message;
+    std::bitset<8> * cover;
+    DWORD mBitCount;
+    DWORD cByteCount;
+    long long current;
 
-        void setMessageByte(BYTE val,int cnt);
-        void setCoverByte(BYTE val,int cnt);
+    void setMessageByte(BYTE val);
+    void setCoverByte(BYTE val,int cnt);
+    void embed(int b,int n);
+    int averageandgetlsbs(int d,int e,int f,int g);
+    int getNbitsFromMessage(int n);
 
-    public:
-        WaveMessageEmbedder(char * message,unsigned int messageSize, BYTE * cover, DWORD coverSize);
-        virtual ~WaveMessageEmbedder();
-        std::bitset<8> * getMessage() { return message; }
-        std::bitset<8> * getCover() { return cover; }
-        void print();
+
+public:
+    int averageNSamples(int n);
+    WaveMessageEmbedder(char * message,unsigned int messageSize, BYTE * cover, DWORD coverSize);
+    virtual ~WaveMessageEmbedder();
+    BYTE * getStegoData(int bitsPerSample,int noOfBytesToAverage);
+    void print();
 
 
 };
--- src/WaveMessageEmbedder.cpp
+++ src/WaveMessageEmbedder.cpp
@@ -6,13 +6,16 @@ using std::endl;
 WaveMessageEmbedder::WaveMessageEmbedder(char * m, unsigned int mSize, BYTE * c, DWORD cSize)
 {
     int cnt;
-    mByteCount = mSize;
+    mBitCount = mSize;
     cByteCount = cSize;
-    message = new std::bitset<8>  [mSize];
-    cover = new std::bitset<8>  [cSize];
+    std::string sm (m);
+    current = 0;
 
+//    std::bitset<(size_t) mSize>  temp (sm);
+    cover = new std::bitset<8>  [cSize];
+    //message = new std::vector<bool>;
     for( cnt = 0 ; cnt < mSize; cnt++)
-        setMessageByte(m[cnt],cnt);
+        setMessageByte(m[cnt]);
 
     for( cnt = 0 ; cnt < cSize; cnt++)
         setCoverByte(c[cnt],cnt);
@@ -21,33 +24,20 @@ WaveMessageEmbedder::WaveMessageEmbedder(char * m, unsigned int mSize, BYTE * c,
 
 WaveMessageEmbedder::~WaveMessageEmbedder()
 {
-    delete [] message;
+//    delete [] message;
     delete [] cover;
 }
 
-void WaveMessageEmbedder::print()
+void WaveMessageEmbedder::setMessageByte(BYTE val)
 {
-    int cnt;
-    cout << "Message Bytes" << endl;
-    for( cnt = 0 ; cnt < mByteCount; cnt++){
-        cout<< message[cnt] << ", ";
-        if ( cnt % 10 == 0)
-            cout << endl;
+    int i;
+    std::bitset<8> bin (val);
+    for(i = 0; i < 8; i++)
+    {
+
+        message.push_back(bin[8 - i]);
+
     }
-
-    cout << "Cover Bytes" << endl;
-    for( cnt = 0 ; cnt < cByteCount; cnt++){
-        cout<< cover[cnt] << ", ";
-        if ( cnt % 10 == 0)
-            cout << endl;
-    }
-
-}
-
-void WaveMessageEmbedder::setMessageByte(BYTE val,int cnt)
-{
-    std::bitset<8> temp (val);
-    message[cnt] = temp;
 }
 
 void WaveMessageEmbedder::setCoverByte(BYTE val,int cnt)
@@ -55,12 +45,90 @@ void WaveMessageEmbedder::setCoverByte(BYTE val,int cnt)
     std::bitset<8> temp (val);
     cover[cnt] = temp;
 }
+
+int WaveMessageEmbedder::getNbitsFromMessage(int n)
+{
+    int i,token = 0;
+
+    for(i = 0; i < n && !message.empty();i++)
+    {
+        token += pow(2,n-i-1)*(int)message.front();
+        message.erase(message.begin());
+
+    }
+    return token;
+
+}
+
+int WaveMessageEmbedder::averageNSamples(int n)
+{
+    int i;
+    int sum = 0;
+    for(  i = current; i < n * 2 + current ; i+=2 )
+    {
+       sum += cover[i].to_ulong() * 256 + cover[i + 1].to_ulong();
+    }
+    current += n;
+    return sum/n;
+
+}
+
+void WaveMessageEmbedder::embed(int b,int n)
+{
+    // get b bits from message
+    int token = getNbitsFromMessage(b);
+    averageNSamples(n);
+    // a = average next set of  n*2 bytes
+    //int averageandgetlsbs(int d,int e,int f,int g);
+    // increment or decrement average until lsb of a = b bit
+    // e = randomly generate
+}
+
+BYTE * WaveMessageEmbedder::getStegoData(int bitsPerSample,int noOfBytesToAverage)
+{
+
+    embed(bitsPerSample,noOfBytesToAverage);
+    //convert cover to BYTE
+
+}
+
+
+void WaveMessageEmbedder::print()
+{
+    int cnt;
+    cout << "Message Bytes"  <<endl;
+
+    for( cnt = 0 ; cnt < mBitCount * 8 ; cnt++){
+        cout<< message[cnt];
+        if ( cnt % 8== 7)
+            cout << ", " ;
+        else if (cnt % 80 == 79)
+            cout << endl;
+    }
+
+    cout << endl;
+
+    cout << "Cover Bytes" << endl;
+    for( cnt = 0 ; cnt < cByteCount; cnt++){
+        cout<< cover[cnt] << ", ";
+        if ( cnt % 10 == 9)
+            cout << endl;
+    }
+
+}
+
+
 int main()
 {
     char message [] = "abcd";
-    BYTE cover []  = {1,255,1,255};
+    BYTE cover []  = {1,0,0,64};
     WaveMessageEmbedder w (message,4,cover,4);
+
+
     w.print();
+    cout << w.averageNSamples(2) << endl;
+
+    //w.print();
 
     return 0;
 }
